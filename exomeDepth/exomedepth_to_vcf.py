67e6c3646b3cf36c5b705642812ec049b8aef4f8
yak1r <yakir.regev@gmail.com>
2020-05-05 14:29:38 +0300

    Write contig headers according to ref

--- exomeDepth/exomedepth_to_vcf.py
+++ exomeDepth/exomedepth_to_vcf.py
@@ -21,23 +21,13 @@ def parse_args():
     return parser.parse_args()
 
 
-def get_vcf_headers(sample_name):
+def get_vcf_headers(sample_name, genome_ref):
     cmdline = " ".join(sys.argv)
     headers = ['##fileformat=VCFv4.1', 'source=exomeDepthVCFConverter,version=0.1.0',
-               f'ConverterCMDLine={cmdline}',
-               "##contig=<ID=1,length=249250621>", "##contig=<ID=2,length=243199373>",
-               "##contig=<ID=3,length=198022430>", "##contig=<ID=4,length=191154276>",
-               "##contig=<ID=5,length=180915260>", "##contig=<ID=6,length=171115067>",
-               "##contig=<ID=7,length=159138663>", "##contig=<ID=8,length=146364022>",
-               "##contig=<ID=9,length=141213431>", "##contig=<ID=10,length=135534747>",
-               "##contig=<ID=11,length=135006516>", "##contig=<ID=12,length=133851895>",
-               "##contig=<ID=13,length=115169878>", "##contig=<ID=14,length=107349540>",
-               "##contig=<ID=15,length=102531392>", "##contig=<ID=16,length=90354753>",
-               "##contig=<ID=17,length=81195210>", "##contig=<ID=18,length=78077248>",
-               "##contig=<ID=19,length=59128983>", "##contig=<ID=20,length=63025520>",
-               "##contig=<ID=21,length=48129895>", "##contig=<ID=22,length=51304566>",
-               "##contig=<ID=X,length=155270560>", "##contig=<ID=Y,length=59373566>",
-               f"#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{sample_name}"]
+               f'ConverterCMDLine={cmdline}']
+    headers += [f"##contig=<ID={name},length={length}>"
+                for name, length in zip(genome_ref.references, genome_ref.lengths)]
+    headers.append(f"#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{sample_name}")
     return headers
 
 
@@ -54,7 +44,7 @@ def main(args):
             open(args.output_path, 'w') as vcf_output:
         is_full_chrom_name = genome_ref.references[0].startswith('chr')
         cnv_reader = csv.DictReader(cnv_input, delimiter='\t')
-        vcf_output.write('\n'.join(get_vcf_headers(sample_name)) + '\n')
+        vcf_output.write('\n'.join(get_vcf_headers(sample_name, genome_ref)) + '\n')
         for cnv_line in cnv_reader:
             vcf_line = get_vcf_line(cnv_line, genome_ref, is_full_chrom_name)
             vcf_output.write(vcf_line + '\n')
